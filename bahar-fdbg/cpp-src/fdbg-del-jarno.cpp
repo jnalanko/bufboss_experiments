ccddc9b15065ddbcdae77c55054d9631886d1445
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-01-27 17:53:16 +0200

    FDBG-del

--- bahar-fdbg/cpp-src/fdbg-del-jarno.cpp
+++ bahar-fdbg/cpp-src/fdbg-del-jarno.cpp
@@ -62,13 +62,12 @@ int main(int argc, char* argv[]) {
     ifstream input;
 
     input.open(argv[1]);
-    unsigned int k = stoi(argv[2]);
+    unsigned int nodeLength = stoi(argv[2]);
+    unsigned int edgemer_k = nodeLength + 1;
     string graphName = argv[3];
 
-    unsigned int nodeLength = k - 1;
-
     BOOST_LOG_TRIVIAL(info) << "reading kmers from new fasta file";
-    getKmers(nKmers, k, kmer_2, input);
+    getKmers(nKmers, edgemer_k, kmer_2, input);
     BOOST_LOG_TRIVIAL(info)
         << "number of kmers for operations: " << kmer_2.size();
 
@@ -83,119 +82,23 @@ int main(int argc, char* argv[]) {
     BOOST_LOG_TRIVIAL(info) << "Original Bits per element:"
                             << Graph.bitSize() / static_cast<double>(Graph.n);
 
-    vector<pair<kmer_t, kmer_t>> nodes;
-    set<kmer_t> added_deleted_nodes;
-    set<pair<kmer_t, kmer_t>> added_deleted_edges;
+    clock_t t_start = clock();
+
+    int64_t n_deleted = 0;
     for (size_t i = 0; i < kmer_2.size(); i++) {
-        string strU = kmer_2[i].substr(0, k - 1);
-        string strV = kmer_2[i].substr(1, k);
+        string strU = kmer_2[i].substr(0, nodeLength);
+        string strV = kmer_2[i].substr(1, nodeLength);
 
         kmer_t u = mer_string_to_binary(strU, num, nodeLength);
         kmer_t v = mer_string_to_binary(strV, num, nodeLength);
-        nodes.push_back(make_pair(u, v));
+        if (Graph.IsEdgeInGraph(u,v)){
+            Graph.dynamicRemoveEdge(u,v);
+        }
     }
 
-    // indexing edges
-    clock_t t_start = clock();
-    for (size_t i = 0; i < nodes.size(); i++) {
-        if (!Graph.IsEdgeInGraph(nodes[i].first, nodes[i].second))
-            added_deleted_edges.insert(
-                make_pair(nodes[i].first, nodes[i].second));
-    }
     double t_elapsed = (clock() - t_start) / CLOCKS_PER_SEC;
-    BOOST_LOG_TRIVIAL(info)
-        << "DONE with indexing all " << kmer_2.size() << " edges";
-    BOOST_LOG_TRIVIAL(info)
-        << "Time per indexing: " << t_elapsed / kmer_2.size();
-    BOOST_LOG_TRIVIAL(info) << kmer_2.size() - added_deleted_edges.size()
-                            << " of the edges were already in the graph";
-
-    /*for (size_t i = 0; i < added_deleted_edges.size() ;i++) {
-            if (!Graph.detect_membership(added_deleted_edges[i].first)) {
-                    added_deleted_nodes.push_back(added_deleted_edges[i].first);}
-                    if (!Graph.detect_membership(added_deleted_edges[i].second))
-       { added_deleted_nodes.push_back(added_deleted_edges[i].second);}
-            }*/
-    for (set<pair<kmer_t, kmer_t>>::iterator it = added_deleted_edges.begin();
-         it != added_deleted_edges.end(); ++it) {
-        if (!Graph.detect_membership(it->first)) {
-            added_deleted_nodes.insert(it->first);
-        }
-        if (!Graph.detect_membership(it->second)) {
-            added_deleted_nodes.insert(it->second);
-        }
-    }
-
-    t_start = clock();
-    for (set<kmer_t>::iterator it = added_deleted_nodes.begin();
-         it != added_deleted_nodes.end(); ++it) {
-        Graph.addNode(*it);
-    }
-    for (set<pair<kmer_t, kmer_t>>::iterator it = added_deleted_edges.begin();
-         it != added_deleted_edges.end(); ++it) {
-        Graph.newDynamicAddEdge(it->first, it->second);
-    }
-
-    t_elapsed = (clock() - t_start) / CLOCKS_PER_SEC;
-    BOOST_LOG_TRIVIAL(info) << "DONE with addition of all "
-                            << added_deleted_edges.size() << " edges";
-    BOOST_LOG_TRIVIAL(info)
-        << "Time per addition: " << t_elapsed / added_deleted_edges.size();
-    BOOST_LOG_TRIVIAL(info)
-        << "After addition graph has " << Graph.n << " nodes ";
-    BOOST_LOG_TRIVIAL(info) << "Bits per element after addition: "
-                            << Graph.bitSize() / static_cast<double>(Graph.n);
-
-    for (set<pair<kmer_t, kmer_t>>::iterator it = added_deleted_edges.begin();
-         it != added_deleted_edges.end(); ++it) {
-        if (!Graph.IsEdgeInGraph(it->first, it->second)) {
-            BOOST_LOG_TRIVIAL(info) << "Addition is not complete";
-            exit(0);
-        }
-    }
-    for (set<kmer_t>::iterator it = added_deleted_nodes.begin();
-         it != added_deleted_nodes.end(); ++it) {
-        if (!Graph.detect_membership(*it)) {
-            BOOST_LOG_TRIVIAL(info) << "Addition is not complete";
-            exit(0);
-        }
-    }
-
-    BOOST_LOG_TRIVIAL(info) << "Addition is complete";
-    t_start = clock();
-    for (set<pair<kmer_t, kmer_t>>::iterator it = added_deleted_edges.begin();
-         it != added_deleted_edges.end(); ++it) {
-        Graph.dynamicRemoveEdge(it->first, it->second);
-    }
-    for (set<kmer_t>::iterator it = added_deleted_nodes.begin();
-         it != added_deleted_nodes.end(); ++it) {
-        Graph.removeNode(*it);
-    }
-
-    t_elapsed = (clock() - t_start) / CLOCKS_PER_SEC;
-    BOOST_LOG_TRIVIAL(info) << "DONE with deletion of all "
-                            << added_deleted_edges.size() << " edges";
-    BOOST_LOG_TRIVIAL(info)
-        << "Time per deletion: " << t_elapsed / added_deleted_edges.size();
-    BOOST_LOG_TRIVIAL(info)
-        << "After deletion graph has " << Graph.n << " nodes ";
+    BOOST_LOG_TRIVIAL(info) << "DONE with deletion of " << n_deleted << " edges";
+    BOOST_LOG_TRIVIAL(info) << "Time per deletion: " << t_elapsed / n_deleted;
     BOOST_LOG_TRIVIAL(info) << "Bits per element after deletion: "
                             << Graph.bitSize() / static_cast<double>(Graph.n);
-
-    for (set<pair<kmer_t, kmer_t>>::iterator it = added_deleted_edges.begin();
-         it != added_deleted_edges.end(); ++it) {
-        if (Graph.IsEdgeInGraph(it->first, it->second)) {
-            BOOST_LOG_TRIVIAL(info) << "Verification did not pass";
-            exit(0);
-        }
-    }
-    for (set<kmer_t>::iterator it = added_deleted_nodes.begin();
-         it != added_deleted_nodes.end(); ++it) {
-        if (Graph.detect_membership(*it)) {
-            BOOST_LOG_TRIVIAL(info) << "Verification did not pass";
-            exit(0);
-        }
-    }
-
-    BOOST_LOG_TRIVIAL(info) << "Verification passed";
 }
