b0628b02140ad61d0ee54253d4dca37d4c8210e3
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-02 19:54:05 +0200

    Clean up

--- run_bufboss.py
+++ run_bufboss.py
@@ -12,61 +12,6 @@ if sys.version_info < (3, 0):
     sys.stdout.write("Error: Python3 required\n")
     sys.exit(1)
 
-def run_get_output(command):
-    # Command can have pipes
-    sys.stderr.write(command + "\n")
-    return subprocess.run(command, shell=True, stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
-
-def run(command):
-    sys.stderr.write(command + "\n")
-    return subprocess.run(command, shell=True)
-
-def drop_path_and_extension(S):
-    return os.path.splitext(os.path.split(S)[1])[0]
-
-def drop_extension(S):
-    return os.path.splitext(S)[0]
-
-def concatenate_to(filelist, destination):
-    for i, file in enumerate(filelist):
-        if i == 0:
-            run("cat " + file + " > " + destination)
-        else:
-            run("cat " + file + " >> " + destination)
-
-# Runs the command and writes to outfile (an opened file object) a line:
-# name: time rss
-# where time is in seconds and rss is in bytes
-def run_timed_rss(command, name, outfile):
-    sys.stderr.write(command + "\n")
-
-    # /usr/bin/time -v prints to stderr
-    stderr_data = subprocess.run("/usr/bin/time -v " + command, shell=True, stderr=subprocess.PIPE).stderr.decode("utf-8").strip()
-    print(stderr_data)
-    rss, time = None, None
-    for line in stderr_data.split('\n'):
-        if "Maximum resident set size (kbytes)" in line:
-            rss = int(line.split()[-1]) * 2**10 # bytes
-        if "Elapsed (wall clock) time (h:mm:ss or m:ss)" in line:
-            token = line.split()[-1]
-            hours, minutes, seconds = 0,0,0
-            if token.count(":") == 1:
-                minutes = float(token.split(":")[0])
-                seconds = float(token.split(":")[1])
-            elif token.count(":") == 2:
-                hours = float(token.split(":")[0])
-                minutes = float(token.split(":")[1])
-                seconds = float(token.split(":")[2])
-            else:
-                print("Error parsing /usr/time/time -v")
-                assert(False)
-            time = hours * 60*60 + minutes*60 + seconds
-    if rss == None or time == None:
-        print("Error parsing /usr/time/time -v")
-        assert(False)
-    outfile.write(name + " " + str(time) + " " + str(rss) + "\n")
-    outfile.flush()
-
 build_program = "bufboss/bin/bufboss_build"
 update_program = "bufboss/bin/bufboss_update"
 query_program = "bufboss/bin/bufboss_query"
