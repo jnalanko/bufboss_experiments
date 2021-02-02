4da9d18af444b226fc4653ac7483bd9c0d5c4316
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-02 22:35:31 +0200

    Enable all bufboss experiments

--- run_bufboss.py
+++ run_bufboss.py
@@ -29,10 +29,10 @@ run("mkdir -p " + deleted)
 query_out = outdir + "/queries.txt"
 resultfile = open("bufboss_results.txt",'w')
 
-run_build = False
-run_add = False
-run_del = False
-run_query = False
+run_build = True
+run_add = True
+run_del = True
+run_query = True
 run_query_vs_buffer_fraction = True
 
 if run_build:
