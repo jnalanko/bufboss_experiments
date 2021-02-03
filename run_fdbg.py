4aff9113c07e041ac577feb408034445f85bdcfd
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-03 23:24:18 +0200

    summary

--- run_fdbg.py
+++ run_fdbg.py
@@ -18,19 +18,19 @@ built = outdir + "/built.dbg"
 added = outdir + "/added.dbg"
 deleted = outdir + "/deleted.dbg"
 
-resultdir = "dynboss_results"
+resultdir = "fdbg_results"
 run("mkdir -p " + resultdir)
 
 # Todo: does it do rc? seems no... build from forward + rc
 
 # Build
-run_to_files("./bahar-fdbg/cpp-src/fdbg-build-jarno " + build_concat_with_rc + " " + str(nodemer_k) + " " + built, resultdir + "/build")
+run_to_files("/usr/bin/time -v ./bahar-fdbg/cpp-src/fdbg-build-jarno " + build_concat_with_rc + " " + str(nodemer_k) + " " + built, resultdir + "/build")
 
 # Add
-run_to_files("./bahar-fdbg/cpp-src/fdbg-add-jarno " + add_concat_with_rc + " " + str(nodemer_k) + " " + built + " " + added, resultdir + "/add")
+run_to_files("/usr/bin/time -v ./bahar-fdbg/cpp-src/fdbg-add-jarno " + add_concat_with_rc + " " + str(nodemer_k) + " " + built + " " + added, resultdir + "/add")
 
 # Del
-#run_to_files("./bahar-fdbg/cpp-src/fdbg-del-jarno " + del_concat_with_rc + " " + str(nodemer_k) + " " + added + " " + deleted, resultdir + "/del")
+#run_to_files("/usr/bin/time -v ./bahar-fdbg/cpp-src/fdbg-del-jarno " + del_concat_with_rc + " " + str(nodemer_k) + " " + added + " " + deleted, resultdir + "/del")
 
 # Query
 for name in query_inputs:
@@ -38,3 +38,19 @@ for name in query_inputs:
     run_to_files("/usr/bin/time -v ./bahar-fdbg/cpp-src/fdbg-query-jarno " + added + " " + filename + " " + outdir + "/" + name + "-result.txt", resultdir + "/" + name)
 
 # ./bahar-fdbg/cpp-src/fdbg-query-jarno fdbg_out/added.dbg data/existing/build_sequence.fasta temp/out.txt
+
+# Parse summary
+summary_out = open(resultdir + "/summary.txt", 'w')
+build_time, build_rss = parse_usr_bin_time(resultdir + "/build.stderr.txt")
+summary_out.write("build " + str(build_time) + " " + str(build_rss) + "\n")
+
+add_time, add_rss = parse_usr_bin_time(resultdir + "/add.stderr.txt")
+summary_out.write("add " + str(add_time) + " " + str(add_rss) + "\n")
+
+#del_time, del_rss = parse_usr_bin_time(resultdir + "/del.stderr.txt")
+#summary_out.write("del " + str(del_time) + " " + str(del_rss) + "\n")
+
+for name in query_inputs:
+    time = parse_our_printed_time(resultdir + "/" + name + ".stderr.txt")
+    rss = -1 # Not available
+    summary_out.write(name + " " + str(time) + " " + str(rss) + "\n")
