ae010c350af0b536b14d01ff43851c4de70fc935
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-03 17:28:27 +0200

    update run_bufboss

--- run_bufboss.py
+++ run_bufboss.py
@@ -37,6 +37,9 @@ run_del = True
 run_query = True
 #run_query_vs_buffer_fraction = True
 
+buf_fractions = [1.0]
+#buf_fractions = [1.0, 0.5, 0.25, 0.1, 0.5, 0.025, 0.01]
+
 if run_build:
     run_to_files("/usr/bin/time -v ./bufboss/KMC/bin/kmc -v -k31 -m1 -ci1 -cs1 -fm " + build_concat + " temp/kmc_db temp", 
                  resultdir + "/build_run_KMC")
@@ -44,8 +47,6 @@ if run_build:
                  resultdir + "/build_BOSS_from_KMC")
 
 if run_add:
-    #buf_fractions = [1.0, 0.5, 0.25, 0.1, 0.5, 0.025, 0.01]
-    buf_fractions = [1.0]
     for b in buf_fractions:
         run_to_files("/usr/bin/time -v " + update_program + " -k " + str(nodemer_k) + " --end-flush -r -b " + str(b) + " -i " + built + " -o " + added + " --add-files " + addlist, resultdir + "/add-" + str(b))
 
@@ -63,3 +64,22 @@ if run_query:
 #    for name in query_inputs:
 #        filename = query_inputs[name]
 #        run_to_files("./bufboss/bin/query_performance_experiment -i " + built + " --add-files " + addlist + " --buf-fraction-increment 0.01 --max-buf-fraction 1.0 -q " + filename + " --tempdir " + tempdir + " --experiment-out " + resultdir + "/buf_frac_query" + name + ".txt")
+
+# Parse summary
+summary_out = open(resultdir + "/summary.txt", 'w')
+KMC_time, KMC_rss = parse_usr_bin_time(resultdir + "/build_run_KMC.stderr.txt")
+build_time, build_rss = parse_usr_bin_time(resultdir + "/build_BOSS_from_KMC.stderr.txt")
+summary_out.write("build " + str(KMC_time + build_time) + " " + str(max(build_rss, KMC_rss)) + "\n")
+
+for b in buf_fractions:
+    add_time, add_rss = parse_usr_bin_time(resultdir + "/add-" + str(b) + ".stderr.txt")
+    summary_out.write("add " + str(add_time) + " " + str(add_rss) + "\n")
+
+for b in buf_fractions:
+    del_time, del_rss = parse_usr_bin_time(resultdir + "/del-" + str(b) + ".stderr.txt")
+    summary_out.write("del " + str(add_time) + " " + str(add_rss) + "\n")
+
+for name in query_inputs:
+    time = parse_our_printed_time(resultdir + "/query-" + name + ".stderr.txt")
+    rss = -1 # Not available
+    summary_out.write("query-" + name + " " + str(time) + " " + str(rss) + "\n")
