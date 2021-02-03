8d175eb237edbb4e7e14ce61dc64ec655839d43e
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-03 23:21:59 +0200

    fdbg queries

--- run_fdbg.py
+++ run_fdbg.py
@@ -22,9 +22,19 @@ resultdir = "dynboss_results"
 run("mkdir -p " + resultdir)
 
 # Todo: does it do rc? seems no... build from forward + rc
+
+# Build
 run_to_files("./bahar-fdbg/cpp-src/fdbg-build-jarno " + build_concat_with_rc + " " + str(nodemer_k) + " " + built, resultdir + "/build")
 
+# Add
 run_to_files("./bahar-fdbg/cpp-src/fdbg-add-jarno " + add_concat_with_rc + " " + str(nodemer_k) + " " + built + " " + added, resultdir + "/add")
 
-run_to_files("./bahar-fdbg/cpp-src/fdbg-del-jarno " + del_concat_with_rc + " " + str(nodemer_k) + " " + added + " " + deleted, resultdir + "/del")
+# Del
+#run_to_files("./bahar-fdbg/cpp-src/fdbg-del-jarno " + del_concat_with_rc + " " + str(nodemer_k) + " " + added + " " + deleted, resultdir + "/del")
 
+# Query
+for name in query_inputs:
+    filename = query_inputs[name]
+    run_to_files("/usr/bin/time -v ./bahar-fdbg/cpp-src/fdbg-query-jarno " + added + " " + filename + " " + outdir + "/" + name + "-result.txt", resultdir + "/" + name)
+
+# ./bahar-fdbg/cpp-src/fdbg-query-jarno fdbg_out/added.dbg data/existing/build_sequence.fasta temp/out.txt
