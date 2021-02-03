7a6bcab0ee9187165dea7c4207a59d0f0ad333ef
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-03 22:56:55 +0200

    fdbg rc

--- run_fdbg.py
+++ run_fdbg.py
@@ -21,10 +21,10 @@ deleted = outdir + "/deleted.dbg"
 resultdir = "dynboss_results"
 run("mkdir -p " + resultdir)
 
-# Todo: does it do rc?
-run_to_files("./bahar-fdbg/cpp-src/fdbg-build-jarno " + build_concat + " " + str(nodemer_k) + " " + built, resultdir + "/build")
+# Todo: does it do rc? seems no... build from forward + rc
+run_to_files("./bahar-fdbg/cpp-src/fdbg-build-jarno " + build_concat_with_rc + " " + str(nodemer_k) + " " + built, resultdir + "/build")
 
-run_to_files("./bahar-fdbg/cpp-src/fdbg-add-jarno " + add_concat + " " + str(nodemer_k) + " " + built + " " + added, resultdir + "/add")
+run_to_files("./bahar-fdbg/cpp-src/fdbg-add-jarno " + add_concat_with_rc + " " + str(nodemer_k) + " " + built + " " + added, resultdir + "/add")
 
-run_to_files("./bahar-fdbg/cpp-src/fdbg-del-jarno " + del_concat + " " + str(nodemer_k) + " " + added + " " + deleted, resultdir + "/del")
+run_to_files("./bahar-fdbg/cpp-src/fdbg-del-jarno " + del_concat_with_rc + " " + str(nodemer_k) + " " + added + " " + deleted, resultdir + "/del")
 
