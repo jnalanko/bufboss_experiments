dee7729bd3dd652268b34d9e0ba9619b6f7c46eb
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-03 17:00:37 +0200

    Update bifrost script

--- run_bifrost.py
+++ run_bifrost.py
@@ -16,9 +16,6 @@ outdir = "bifrost_out"
 run("mkdir -p " + outdir)
 
 program = "bifrost/build/src/Bifrost"
-#buildlist = datadir + "/coli12_build.txt"
-#addlist = datadir + "/coli12_add.txt"
-#queryfile = "data/reads/coli_reads_half1.fasta"
 
 # Bifrost appends .gfa to these
 built = outdir + "/built.dbg"
@@ -26,16 +23,13 @@ added = outdir + "/added.dbg"
 deleted = outdir + "/deleted.dbg"
 query_out = outdir + "/queries.txt"
 
-resultfile = open("bifrost_results.txt",'w')
+resultdir = "bifrost_results"
+run("mkdir -p " + resultdir)
 
 # Bifrost is node-centric and nodes are k-mers.
-run_timed_rss(program + " build -r " + buildlist + " -k 30 -y -o " + built, "bifrost-build", resultfile)
-run_timed_rss(program + " update -g " + built+".gfa" + " -r " + addlist + " -k 30 -o " + added, "bifrost-update", resultfile)
+run_to_files("/usr/bin/time -v " + program + " build -r " + buildlist + " -k 30 -y -o " + built, resultdir + "/build")
+run_to_files("/usr/bin/time -v " + program + " update -g " + built+".gfa" + " -r " + addlist + " -k 30 -o " + added, resultdir + "/add")
 
-run_timed_rss(program + " query -g " + added+".gfa" + " -q " + query_existing_build_edgemers + " -o " + query_out + " --ratio-kmers 1", "bifrost-query-existing_build_edgemers", resultfile)
-run_timed_rss(program + " query -g " + added+".gfa" + " -q " + query_existing_build_sequence + " -o " + query_out + " --ratio-kmers 1", "bifrost-query-existing_build_seq", resultfile)
-run_timed_rss(program + " query -g " + added+".gfa" + " -q " + query_existing_added_edgemers + " -o " + query_out + " --ratio-kmers 1", "bifrost-query-existing_added_edgemers", resultfile)
-run_timed_rss(program + " query -g " + added+".gfa" + " -q " + query_existing_added_sequence + " -o " + query_out + " --ratio-kmers 1", "bifrost-query-existing-added-seq", resultfile)
-
-run_timed_rss(program + " query -g " + added+".gfa" + " -q " + query_random_edgemers + " -o " + query_out + " --ratio-kmers 1", "bifrost-query-random_edgemers", resultfile)
-run_timed_rss(program + " query -g " + added+".gfa" + " -q " + query_random_sequence + " -o " + query_out + " --ratio-kmers 1", "bifrost-query-random_seq", resultfile)
+for name in query_inputs:
+    filename = query_inputs[name]
+    run_to_files("/usr/bin/time -v " + program + " query -g " + added+".gfa" + " -q " + filename + " -o " + query_out + " --ratio-kmers 1", resultdir + "/query-" + name)
