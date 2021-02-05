88207de220e509baffc48e1e8bd77875b80ff531
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-05 11:45:17 +0200

    Fix run_bifrost

--- run_bifrost.py
+++ run_bifrost.py
@@ -43,11 +43,11 @@ if run_query:
 # Parse summary
 summary_out = open(resultdir + "/summary.txt", 'w')
 build_time, build_rss = parse_usr_bin_time(resultdir + "/build.stderr.txt")
-build_disk = get_disk_size_bytes(built)
+build_disk = get_disk_size_bytes(built + ".gfa")
 summary_out.write("build " + str(build_time) + " " + str(build_rss) + " " + str(build_disk) + "\n")
 
 add_time, add_rss = parse_usr_bin_time(resultdir + "/add.stderr.txt")
-add_disk = get_disk_size_bytes(added)
+add_disk = get_disk_size_bytes(added + ".gfa")
 summary_out.write("add " + str(add_time) + " " + str(add_rss) + " " + str(add_disk) + "\n")
 
 for name in query_inputs:
