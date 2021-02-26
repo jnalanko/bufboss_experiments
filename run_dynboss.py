e69687846abc18d0e819f7d9fa9f3119f4a80585
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-02-26 19:42:09 +0200

    Refactor dynboss script

--- run_dynboss.py
+++ run_dynboss.py
@@ -11,7 +11,11 @@ if sys.version_info < (3, 0):
     sys.stdout.write("Error: Python3 required\n")
     sys.exit(1)
 
-outdir = "dynboss_out"
+config = ExperimentConfig()
+config.load(sys.argv[1])
+experiment_dir = config.experiment_dir
+
+outdir = experiment_dir + "/dynboss_out"
 run("mkdir -p " + outdir)
 
 program = "dynboss/bin/dynamicBOSS-jarno"
@@ -20,25 +24,25 @@ built = outdir + "/built.dbg"
 added = outdir + "/added.dbg"
 deleted = outdir + "/deleted.dbg"
 
-resultdir = "dynboss_results"
+resultdir = experiment_dir + "/dynboss_results"
 run("mkdir -p " + resultdir)
 
 # Count k-mers. We use the reverse-complement concatenated data because dynboss does not index rc by itself.
-run_to_files("/usr/bin/time -v dynboss/dsk-1.6906/dsk " + build_concat_with_rc + " " + str(edgemer_k), resultdir + "/dsk")
-run_to_files("/usr/bin/time -v dynboss/bin/cosmo-pack " + drop_path_and_extension(build_concat_with_rc) + ".solid_kmers_binary", resultdir + "/pack")
+run_to_files("/usr/bin/time -v dynboss/dsk-1.6906/dsk " + config.buildfile_rc + " " + str(edgemer_k), resultdir + "/dsk")
+run_to_files("/usr/bin/time -v dynboss/bin/cosmo-pack " + drop_path_and_extension(config.buildfile_rc) + ".solid_kmers_binary", resultdir + "/pack")
 
 # Build
-run_to_files("/usr/bin/time -v " + program + " build -p " + drop_path_and_extension(build_concat_with_rc) + ".solid_kmers_binary.packed -o " + built, resultdir + "/build")
+run_to_files("/usr/bin/time -v " + program + " build -p " + drop_path_and_extension(config.buildfile_rc) + ".solid_kmers_binary.packed -o " + built, resultdir + "/build")
 
 # Add
-run_to_files("/usr/bin/time -v " + program + " add -g " + built + " -s " + add_concat_with_rc + " -o " + added, resultdir + "/add")
+run_to_files("/usr/bin/time -v " + program + " add -g " + built + " -s " + config.addfile_rc + " -o " + added, resultdir + "/add")
 
 # Del
-run_to_files("/usr/bin/time -v " + program + " delete -g " + added + " -s " + del_concat_with_rc + " -o " + deleted, resultdir + "/del")
+run_to_files("/usr/bin/time -v " + program + " delete -g " + added + " -s " + config.delfile_rc + " -o " + deleted, resultdir + "/del")
 
 # Query
-for name in query_inputs:
-    filename = query_inputs[name]
+for name in config.query_inputs:
+    filename = config.query_inputs[name]
     run_to_files("/usr/bin/time -v " + program + " query -g " + built + " -s " + filename + " --query-result " + outdir + "/" + name + "-result.txt", resultdir + "/" + name)
 
 # Parse summary
@@ -57,7 +61,7 @@ del_time, del_rss = parse_usr_bin_time(resultdir + "/del.stderr.txt")
 del_disk = get_disk_size_bytes(deleted)
 summary_out.write("del " + str(del_time) + " " + str(del_rss) + " " + str(del_disk) + "\n")
 
-for name in query_inputs:
+for name in config.query_inputs:
     time = parse_our_printed_time(resultdir + "/" + name + ".stderr.txt")
     rss = -1 # Not available
     summary_out.write(name + " " + str(time) + " " + str(rss) + "\n")
